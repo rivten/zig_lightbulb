9e69a91eb3611f67d1aaa1338288f6d1cef3073f
rivten <hugo.viala@gmail.com>
2019-04-25 14:07:57 +0200

    add xoroshiro random generator

--- code/lightbulb.zig
+++ code/lightbulb.zig
@@ -1,6 +1,53 @@
 const std = @import("std");
+const warn = std.debug.warn;
+
+const prisonner = struct {};
+
+const PrisonnerCount = 100;
+
+const random_series = struct {
+    Alpha: u64,
+    Beta: u64,
+};
+
+fn RandomSeed(Alpha: u64, Beta: u64) random_series {
+    const Series = random_series{ .Alpha = Alpha, .Beta = Beta };
+    return (Series);
+}
+
+fn RotateLeft(Value: u64, Rotate: u6) u64 {
+    if (Rotate == 0) {
+        return (Value);
+    } else {
+        return ((Value << Rotate) | (Value >> (0b111111 - (Rotate - 1))));
+    }
+}
+
+fn RandomNextU64(Series: *random_series) u64 {
+    var Alpha = Series.Alpha;
+    var Beta = Series.Beta;
+    var Result: u64 = 0;
+    _ = @addWithOverflow(u64, Alpha, Beta, &Result);
+
+    Beta = Beta ^ Alpha;
+    Series.Alpha = RotateLeft(Alpha, 24) ^ Beta ^ (Beta << 16);
+    Series.Beta = RotateLeft(Beta, 37);
+
+    return (Result);
+}
+
+test "xoroshiro" {
+    var Series = RandomSeed(1234, 5678);
+    var i: usize = 0;
+    while (i < 100) : (i += 1) {
+        const RandomValue = RandomNextU64(&Series);
+        warn("{}\n", RandomValue);
+    }
+}
 
 pub fn main() !void {
     var stdout = try std.io.getStdOut();
     try stdout.write("Hello world!\n");
+
+    var PrisonnerTaken = []bool{false} ** PrisonnerCount;
 }
